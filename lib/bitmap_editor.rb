28f7719eaf33f460f0bce5aa97dce2fe4547d668
Mukesh <mukpdev11@gmail.com>
2020-12-10 21:05:12 +0530

    update bitmap_editor for supporting bitmap

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -1,16 +1,27 @@
+require './lib/bitmap'
+
 class BitmapEditor
 
   def run(file)
     return puts "please provide correct file" if file.nil? || !File.exists?(file)
 
+    bitmap = Bitmap.new
     File.open(file).each do |line|
-      line = line.chomp
-      case line
-      when 'S'
-          puts "There is no image"
-      else
-          puts 'unrecognised command :('
-      end
+      params = line.chomp
+                   .split(' ')
+                   .map { |e| e.match?(/\d+/)? e.to_i : e }
+
+      action = case params.shift
+        when 'I' then :create
+        when 'C' then :clear_bitmap
+        when 'L' then :pixel_color
+        when 'V' then :vertical_color
+        when 'H' then :horizontal_color
+        when 'S' then :to_s
+        else :unrecognised
+        end
+
+      bitmap.public_send(action, *params)
     end
   end
 end
