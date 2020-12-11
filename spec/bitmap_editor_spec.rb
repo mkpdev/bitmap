f2ddc6634711830ac1c8fc483e15d8aca921d09c
Mukesh <mukpdev11@gmail.com>
2020-12-11 17:53:50 +0530

    add spec and example

---
+++ spec/bitmap_editor_spec.rb
@@ -0,0 +1,22 @@
+require './lib/bitmap_editor'
+
+class BitmapEditorSpec
+
+  describe ".BitmapEditor" do
+    subject { BitmapEditor.new }
+    
+    it "should return expected result of file examples/show.txt" do
+      subject = BitmapEditor.new
+      result  = subject.run("examples/show.txt").bitmap.collect(&:join).join("\n")
+
+      expect(result).to eq("ooooo\nooZZZ\nAWooo\noWooo\noWooo\noWooo")
+    end
+
+    it "should return expected result of file examples/show-1.txt" do
+      subject = BitmapEditor.new
+      result  = subject.run("examples/show-1.txt").bitmap.collect(&:join).join("\n")
+
+      expect(result).to eq("oFFFFo\noPoooo\noPoooo\noPoooo\noPoooo\nooooQo\nooooQo\nooooQo")
+    end
+  end
+end
